0bc055e3402bfea7c483cc1a951cbcd89ba14ef2
Sagnik <santrasagnik10@gmail.com>
2023-02-04 02:17:27 +0530

    Changed frontend

---
+++ client/src/components/Timeline/Timeline.js
@@ -0,0 +1,147 @@
+import React from "react"
+import "./Timeline.css"
+
+const Timeline = () => {
+    return (
+        <section className="timeline">
+            <ul>
+              <li>
+                <div className="timeline-content">
+                    <h2 className="date">17th February 2023</h2>
+                    <div className="time-box">
+                        <h5>Welcome to MES 2023</h5>
+                        <p>Launch ~ Director, MIT</p>
+                    </div>
+                    <div className="time-box">
+                        <h5>Workshop Name</h5>
+                        <p>Workshop by Industry Pro</p>
+                    </div>
+                </div>
+              </li>
+              <li>
+                <div className="timeline-content">
+                    <h2 className="date">18th to 22th February 2023</h2>
+                    <div className="time-box">
+                        <h5>Workshop/Webinars</h5>
+                        <p>Workshop and webinars to be taken by experienced industry professionals with the goal of having learned something when students come out of the auditorium</p>
+                    </div>
+                </div>
+              </li>
+              <li>
+                <div className="timeline-content">
+                    <h2 className="date">23th February 2023</h2>
+                    <div className="time-box">
+                        <h5>Keynote Session</h5>
+                        <p></p>
+                    </div>
+                    <div className="time-box">
+                        <h5>Youth Panel Interaction</h5>
+                        <p></p>
+                    </div>
+                    <div className="time-box">
+                        <h5>Case Maze</h5>
+                        <p></p>
+                    </div>
+                </div>
+              </li>
+              <li>
+                <div className="timeline-content">
+                    <h2 className="date">24th February 2023</h2>
+                    <div className="time-box">
+                        <h5>PitchTank</h5>
+                        <p></p>
+                    </div>
+                    <div className="time-box">
+                        <h5>MoneyQuest</h5>
+                        <p></p>
+                    </div>
+                    <div className="time-box">
+                        <h5>Panel Discussion</h5>
+                        <p></p>
+                    </div>
+                    <div className="time-box">
+                        <h5>Networking Dinner</h5>
+                    </div>
+                </div>
+              </li>
+              <li>
+                <div className="timeline-content">
+                    <h2 className="date">25th February 2023</h2>
+                    <div className="time-box">
+                        <h5>InnnovationMela</h5>
+                        <p></p>
+                    </div>
+                    <div className="time-box">
+                        <h5>Fallout</h5>
+                        <p></p>
+                    </div>
+                    <div className="time-box">
+                        <h5>MoneyBall</h5>
+                        <p></p>
+                    </div>
+                    <div className="time-box">
+                        <h5>Entertainment Night</h5>
+                    </div>
+                </div>
+              </li>
+              {/* <li>
+                <div className="timeline-content">
+                    <h2 className="date">30th March 2022</h2>
+                    <div className="time-box">
+                        <h5>Presentation</h5>
+                        <p>Case Maze Top 5 presentations</p>
+                    </div>
+                    {/* <div className="time-box">
+                        <h5>Movie Night</h5>
+                        <p>Movie Night at FC2 Amphitheatre</p>
+                    </div> 
+                </div>
+              </li>
+              <li>
+                <div className="timeline-content">
+                    <h2 className="date">1st April 2022</h2>
+                    <div className="time-box">
+                        <h5>2.30pm to 5.45 pm</h5>
+                        <p>Panel Discussion on Entrepreneurship Essentials, finance and business</p>
+                    </div>
+                    <div className="time-box">
+                        <h5>5.45pm to 7pm</h5>
+                        <p>Pitch Tank</p>
+                    </div>
+                    <div className="time-box">
+                        <h5>7pm to 8.30pm</h5>
+                        <p>Networking Arena</p>
+                    </div>
+                    <div className="time-box">
+                        <h5>4.45pm to 8pm</h5>
+                        <p>Money Quest</p>
+                    </div>
+                </div>
+              </li>
+              <li>
+                <div className="timeline-content">
+                    <h2 className="date">2nd April 2022</h2>
+                    <div className="time-box">
+                        <h5>11am</h5>
+                        <p>Keynote session</p>
+                    </div>
+                    <div className="time-box">
+                        <h5>10am to 4pm</h5>
+                        <p>Startup Fair</p>
+                    </div>
+                    <div className="time-box">
+                        <h5>2.30pm to 4.30pm</h5>
+                        <p>Visit to Manipal Incubators</p>
+                    </div>
+                    <div className="time-box">
+                        <h5>7pm</h5>
+                        <p>Standup Comedy Show</p>
+                    </div>
+                </div>
+              </li> */}
+            </ul>
+        </section>
+    )
+}
+
+export default Timeline
